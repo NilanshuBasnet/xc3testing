8abc7c9a9988bda6c48fb43ea67a8c852891f8df
NilanshuBasnet <46049944+NilanshuBasnet@users.noreply.github.com>
2024-03-20 10:31:42 +1100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,10 @@
+# Module Declaration
+# ------------------
+# This declares the usage of the "serverless" module to provision serverless resources.
+
+module "serverless" {
+  # Module Source
+  # -------------
+  # Specifies the path to the directory containing the Terraform configuration for the "serverless" module.
+  source = "./modules/serverless"
+}
